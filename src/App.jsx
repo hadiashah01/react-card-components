d33121e91c6e085acc22dafa1631caa27ddd7ac2
Hadia <hadiashah010@gmail.com>
2026-09-12 04:47:17 +0500

    feat: implement job openings display with Card component and styling updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,137 @@
 import Card from "./components/Card";
+import "./App.css";
 
 const App = () => {
-  return <Card />;
+  const jobOpenings = [
+    {
+      brandlogo:
+        "https://cdn.jsdelivr.net/npm/simple-icons@v13/icons/google.svg",
+      company: "Google",
+      datePosted: "2 days ago",
+      post: "Frontend Developer",
+      tag1: "Full Time",
+      tag2: "Junior Level",
+      pay: "$42/hour",
+      location: "Bengaluru, India",
+    },
+    {
+      brandlogo:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSb8ambll_L5-YV4PTuRhSwTlRXhbK1jQCs0n6VKkYRKA&s",
+      company: "Microsoft",
+      datePosted: "5 days ago",
+      post: "Software Engineer",
+      tag1: "Full Time",
+      tag2: "Mid Level",
+      pay: "$48/hour",
+      location: "Hyderabad, India",
+    },
+    {
+      brandlogo:
+        "https://cdn.jsdelivr.net/npm/simple-icons@v13/icons/amazon.svg",
+      company: "Amazon",
+      datePosted: "1 week ago",
+      post: "Frontend Engineer",
+      tag1: "Full Time",
+      tag2: "Junior Level",
+      pay: "$40/hour",
+      location: "Bengaluru, India",
+    },
+    {
+      brandlogo:
+        "https://cdn.jsdelivr.net/npm/simple-icons@v13/icons/apple.svg",
+      company: "Apple",
+      datePosted: "3 days ago",
+      post: "UI Engineer",
+      tag1: "Full Time",
+      tag2: "Mid Level",
+      pay: "$50/hour",
+      location: "Hyderabad, India",
+    },
+    {
+      brandlogo: "https://cdn.jsdelivr.net/npm/simple-icons@v13/icons/meta.svg",
+      company: "Meta",
+      datePosted: "4 days ago",
+      post: "React Developer",
+      tag1: "Full Time",
+      tag2: "Junior Level",
+      pay: "$45/hour",
+      location: "London, UK",
+    },
+    {
+      brandlogo:
+        "https://cdn.jsdelivr.net/npm/simple-icons@v13/icons/nvidia.svg",
+      company: "NVIDIA",
+      datePosted: "2 weeks ago",
+      post: "Software Developer",
+      tag1: "Full Time",
+      tag2: "Senior Level",
+      pay: "$62/hour",
+      location: "Austin, USA",
+    },
+    {
+      brandlogo:
+        "https://cdn.jsdelivr.net/npm/simple-icons@v13/icons/netflix.svg",
+      company: "Netflix",
+      datePosted: "10 days ago",
+      post: "Frontend Engineer",
+      tag1: "Full Time",
+      tag2: "Senior Level",
+      pay: "$65/hour",
+      location: "Los Angeles, USA",
+    },
+    {
+      brandlogo:
+        "https://cdn.jsdelivr.net/npm/simple-icons@v13/icons/adobe.svg",
+      company: "Adobe",
+      datePosted: "3 weeks ago",
+      post: "UI Developer",
+      tag1: "Part Time",
+      tag2: "Junior Level",
+      pay: "$35/hour",
+      location: "Noida, India",
+    },
+    {
+      brandlogo:
+        "https://cdn.jsdelivr.net/npm/simple-icons@v13/icons/salesforce.svg",
+      company: "Salesforce",
+      datePosted: "1 week ago",
+      post: "Web Developer",
+      tag1: "Full Time",
+      tag2: "Mid Level",
+      pay: "$44/hour",
+      location: "San Francisco, USA",
+    },
+    {
+      brandlogo:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnFC8b4Y36NgmC8OJbWDlVXFenH3_h2bPBQI1wL0yfQw&s=10",
+      company: "IBM",
+      datePosted: "10 weeks ago",
+      post: "Frontend Developer",
+      tag1: "Full Time",
+      tag2: "Junior Level",
+      pay: "$38/hour",
+      location: "Bengaluru, India",
+    },
+  ];
+
+  return (
+    <div className="parent flex">
+      {jobOpenings.map((jobOpening) => {
+        return (
+          <Card
+            datePosted={jobOpening.datePosted}
+            post={jobOpening.post}
+            company={jobOpening.company}
+            logo={jobOpening.brandlogo}
+            tag1={jobOpening.tag1}
+            tag2={jobOpening.tag2}
+            pay={jobOpening.pay}
+            location={jobOpening.location}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,12 +2,12 @@ import "./card.css";
 import React from "react";
 import { Bookmark } from "lucide-react";
 
-const Card = () => {
+const Card = (jobOpening) => {
   return (
     <div className="card">
       <section className="header flex">
         <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZciLmZ88o0cLKtDAqEAvqA1fHAYVb2MXxcJl_VOLRqw&s=10"
+          src={jobOpening.logo}
                   alt="Company Logo"
                   className="logo"
         />
@@ -17,17 +17,17 @@ const Card = () => {
       </section>
       <section className="main">
         <div className="main-top flex">
-          <h3 className="company-name">Amazon</h3>
-          <span className="date-posted">5 days ago</span>
+          <h3 className="company-name">{jobOpening.company}</h3>
+          <span className="date-posted">{jobOpening.datePosted}</span>
         </div>
-        <h2 className="role">Senior UI/UX Designer</h2>
-        <span className="type">Part-Time</span>
-        <span className="key-feature">Senior Level</span>
+        <h2 className="role">{jobOpening.post}</h2>
+        <span className="type">{jobOpening.tag1}</span>
+              <span className="key-feature">{ jobOpening.tag2}</span>
       </section>
       <section className="footer flex">
         <div className="details flex flex--column">
-          <h4 className="rate">$120/hr</h4>
-          <div className="location">Mumbai, India</div>
+                  <h4 className="rate">{jobOpening.pay}</h4>
+          <div className="location">{jobOpening.location}</div>
         </div>
         <button className="cta">Apply Now</button>
       </section>
